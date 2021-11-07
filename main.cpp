7f31c4a16a9b1c010fa0b9c72bbd1a33f83840af
McKelvey-Connor <cmckelvey@nevada.unr.edu>
2021-11-07 15:58:26 -0800

    idfk

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Connor McKelvey on 11/7/2021.
+#include <iostream>
+
+int main(int argc,char** arg){
+    if(argc!=3){
+        cout<<"bro. you need more info"<<std::endl;
+    }
+    return 0;
+}
+//
+
