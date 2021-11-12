72218ba7625b44141651826381eb255c1b143f8e
McKelvey-Connor <cmckelvey@nevada.unr.edu>
2021-11-11 18:10:41 -0800

    need to move to UI

--- main.cpp
+++ main.cpp
@@ -3,8 +3,28 @@
 #include <iostream>
 
 int main(int argc,char** arg){
+    int menuChoice;
     if(argc!=3){
         cout<<"bro. you need more info"<<std::endl;
+        switch (menuChoice){
+            case 1:
+                //display file info
+                break;
+            case 2:
+                //echo
+                break;
+            case 3:
+                //normalization
+                break;
+            case 4:
+                //gain
+                break;
+            case 0:
+                //end loop
+                break;
+
+
+        }
     }
     return 0;
 }
