6e9465602fc048e57cf7410f0de7f89436e188a2
McKelvey-Connor <cmckelvey@nevada.unr.edu>
2021-11-15 16:14:08 -0800

    need to move to UI

--- main.cpp
+++ main.cpp
@@ -2,31 +2,5 @@
 // Created by Connor McKelvey on 11/7/2021.
 #include <iostream>
 
-int main(int argc,char** arg){
-    int menuChoice;
-    if(argc!=3){
-        cout<<"bro. you need more info"<<std::endl;
-        switch (menuChoice){
-            case 1:
-                //display file info
-                break;
-            case 2:
-                //echo
-                break;
-            case 3:
-                //normalization
-                break;
-            case 4:
-                //gain
-                break;
-            case 0:
-                //end loop
-                break;
-
-
-        }
-    }
-    return 0;
-}
 //
 
--- userinterface.cpp
+++ userinterface.cpp
@@ -8,3 +8,29 @@ wav_filecopy.cpp
 wav_filecopy.h
 wav_file.cpp
 wav_file.h
+
+int main(int argc,char** arg){
+    int menuChoice;
+    if(argc!=3){
+        cout<<"bro. you need more info"<<std::endl;
+        switch (menuChoice){
+            case 1:
+                //display file info
+                break;
+            case 2:
+                //echo
+                break;
+            case 3:
+                //normalization
+                break;
+            case 4:
+                //gain
+                break;
+            case 0:
+                //end loop
+                break;
+
+
+        }
+    }
+    return 0;
