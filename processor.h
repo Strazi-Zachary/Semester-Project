beac32cb626d8c33607ebb32fdbb703af215f425
Zachary Strazi <zstrazi@nevada.unr.edu>
2021-11-30 19:43:14 -0800

    Final Draft

--- processor.h
+++ processor.h
@@ -5,12 +5,16 @@
 #include <cmath>
 #include <cstdlib>
 
+//Abstract Class Processor
 class Processor{
 	
 public:
 		
 	Processor() = default;		
 	
+	/*
+	* Mono and stereo virtual functions @ param buffer, buffersize
+	*/
 	virtual void monoBuffer(unsigned char* buffer, int bufferSize) = 0;
 	virtual void monoBuffer(signed short* buffer, int bufferSize) = 0;
 	virtual void stereoBuffer(unsigned char* buffer, int bufferSize) = 0;
